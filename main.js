cb74baf9fe93ab9b7204e6a129143bd4a259c8ef
Barry <barrym07@gmail.com>
2021-09-27 09:23:24 -0600

    computerChoice logic programmed

--- main.js
+++ main.js
@@ -2,3 +2,42 @@ const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv;
 
+class Game {
+  constructor(choice, computerChoice) {
+    this.computerChoice = computerChoice;
+    this.choice = choice;
+    // this.options = ['rock', 'paper', 'scissors'] ths can probably go, not used
+
+  }
+  outcome(choice, computerChoice) {
+    if (this.choice = 'rock' this.computerChoice = 'paper') {
+      //if( this)
+      console.log(argv., 'Paper beats Rock')
+
+    }
+
+
+  }
+
+}
+let computerChoice = Math.ceil(Math.random() * 3);
+if (computerChoice = 1) {
+  computerChoice = 'rock';
+} else if (computerChoice = 2) {
+  computerChoice = 'paper';
+} else {
+  computerChoice = 'scissors';
+}
+
+
+
+
+
+
+
+
+// if (argv.move > 3 && argv.distance < 53.5) {
+//   console.log('Plunder more riffiwobbles!')
+// } else {
+//   console.log('Retreat from the xupptumblers!')
+// }
