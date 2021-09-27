af8a6c89a4049d96aa2497cb449d8841442ae1aa
Barry <barrym07@gmail.com>
2021-09-27 10:55:17 -0600

    accepts user input and generates computerchoice

--- main.js
+++ main.js
@@ -2,42 +2,114 @@ const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv;
 
-class Game {
-  constructor(choice, computerChoice) {
-    this.computerChoice = computerChoice;
-    this.choice = choice;
-    // this.options = ['rock', 'paper', 'scissors'] ths can probably go, not used
+const choice = argv.choice;
+//const computerChoice = output;
 
+
+class Computer {
+  constructor() {
+    this.output = "";
+
+    let computerLogic = Math.ceil(Math.random() * 3);
+    console.log("computer logic:", computerLogic);
+
+    if (computerLogic === 1) {
+      this.output = 'rock';
+    } else if (computerLogic === 2) {
+      this.output = 'paper';
+    } else {
+      this.output = 'Scissors';
+    }
+    //return this.computerChoice;
+    console.log(computerLogic);
+    console.log(this.output);
   }
-  outcome(choice, computerChoice) {
-    if (this.choice = 'rock' this.computerChoice = 'paper') {
-      //if( this)
-      console.log(argv., 'Paper beats Rock')
+  //return output;
+}
+newComp = new Computer;
 
+
+class Game {
+  constructor(choice) {
+    // this.computerChoice = [];
+    this.choice = choice;
+    this.computerChoice = newComp.output;
+    console.log("Player plays:", choice);
+    console.log("Computer plays:", this.computerChoice);
+
+    // this.options = ['rock', 'paper', 'scissors'] ths can probably go, not used
+  }
+
+  option(choice) {
+    if (this.choice = 'rock') {
+      Rock.logic;
+      console.log("rock logic chosen")
+    } else if (this.choice = 'paper') {
+      Paper.logic;
+    } else {
+      Scissors.logic;
     }
 
-
   }
 
+  // computer() {
+  //   let computerLogic = Math.ceil(Math.random() * 3);
+  //   if (computerLogic = 1) {
+  //     return this.computerChoice.push('rock');
+  //   } else if (computerLogic = 2) {
+  //     return this.computerChoice.push('paper');
+  //   } else {
+  //     return this.computerChoice.push('Scissors');
+  //   }
+  //   //return this.computerChoice;
+  //   console.log(this.computerLogic);
+  //   console.log(this.computerChoice);
+  // }
+
+
 }
-let computerChoice = Math.ceil(Math.random() * 3);
-if (computerChoice = 1) {
-  computerChoice = 'rock';
-} else if (computerChoice = 2) {
-  computerChoice = 'paper';
-} else {
-  computerChoice = 'scissors';
+
+class Rock extends Game {
+  super(choice, computerChoice) { }
+
+  logic() {
+    //if else statements
+    console.log("this is the from the rock logic")
+  }
+
+
+
 }
 
+class Paper extends Game {
+  super(choice, computerChoice) { }
+
+  logic() {
+    //if else statements
+    console.log("this is the from the paper logic")
+  }
+
+
+
+}
+
+class Scissors extends Game {
+  super(choice, computerChoice) { }
+
+  logic() {
+    //if else statements
+    console.log("this is the from the scissors logic")
+  }
+
+
+
+}
+//instantiate new game
+newGame = new Game(choice);
+//newGame.computer();
 
 
 
 
 
 
-
-// if (argv.move > 3 && argv.distance < 53.5) {
-//   console.log('Plunder more riffiwobbles!')
-// } else {
-//   console.log('Retreat from the xupptumblers!')
-// }
